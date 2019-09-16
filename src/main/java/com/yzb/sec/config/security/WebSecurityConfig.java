0601bc20fe5b74132f224cdab3629e93821f03b9
wangban <yzbbanban@live.com>
2019-09-16 10:24:55 +0800

    生成jwt，多余的去掉

--- src/main/java/com/yzb/sec/config/security/WebSecurityConfig.java
+++ src/main/java/com/yzb/sec/config/security/WebSecurityConfig.java
@@ -1,16 +1,12 @@
 package com.yzb.sec.config.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.yzb.sec.common.util.SysManageUserUtils;
 import com.yzb.sec.config.redis.RedisClusterClient;
-import com.yzb.sec.domain.result.ResultJson;
 import com.yzb.sec.service.impl.SysMangeServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.*;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -18,11 +14,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.io.PrintWriter;
 
 /**
  * @author wangban
